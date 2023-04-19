b01bcee8d4568f88379fadd667090054787dc508
mbaranturkmen <kilexey@gmail.com>
2023-04-19 17:18:04 +0100

    rm: duplicate ingress SG and ports from 0 to 0

--- main.tf
+++ main.tf
@@ -86,13 +86,6 @@ resource "aws_security_group" "public_sg" {
     cidr_blocks = ["0.0.0.0/0"] # any ip address can access it
   }
 
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 65535
@@ -100,13 +93,6 @@ resource "aws_security_group" "public_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 }
 
 # Associate a subnet with a route table
